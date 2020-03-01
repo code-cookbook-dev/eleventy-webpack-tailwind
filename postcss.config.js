f633c78f25f7a7c0161686e2c942bc61eba5cfb1
Akbar Ibrahim <iakbar@gmail.com>
2020-03-01 19:47:31 +0530

    Configure PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,22 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
+const isDev = process.env.APP_ENV === "development";
+
 const plugins = [
   tailwindcss('tailwind.config.js'),
   autoprefixer,
 ];
+
+if (!isDev) {
+  const purgecss = require('@fullhuman/postcss-purgecss');
+
+  [].push.apply(plugins, [
+    purgecss({
+      content: ['src/**/*.njk', 'src/**/*.md', 'src/**/*.js'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+  ]);
+}
 
 module.exports = { plugins };
