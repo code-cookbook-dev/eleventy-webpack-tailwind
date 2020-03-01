d11f4b2c5636113c88803e23ff1141ff7aec2aca
Akbar Ibrahim <iakbar@gmail.com>
2020-03-01 16:40:24 +0530

    Configure Eleventy directories

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'data',
+    }
+  };
+};
